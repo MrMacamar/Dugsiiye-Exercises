e819ceeffef8d931a0d07a055c25335194df0b31
MrMacamar <macamartech@gmail.com>
2026-03-17 23:17:31 +0100

    Exercise 1

---
+++ Js Exercise - 1/main.js
@@ -0,0 +1,19 @@
+// Arithmetic Exercise
+
+let x = 7;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+
+
+
+// Pre increment
+
+
+// Post increment
+
