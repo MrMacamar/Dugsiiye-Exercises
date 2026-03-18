1a15bd8909304105d41f7d54091d68d2a28fc5b9
MrMacamar <macamartech@gmail.com>
2026-03-18 18:03:14 +0100

    exercise 5

---
+++ Js exercise - 5/main5.js
@@ -0,0 +1,12 @@
+// Arrow function
+
+const add = (a,b) => {
+    return a + b;
+}
+
+console.log("Your total sum are: " + add(9,70));
+console.log("Your total sum are: " + add(9,80));
+console.log("Your total sum are: " + add(9,90));
+console.log("Your total sum are: " + add(9,100));
+console.log("Your total sum are: " + add(9,200));
+
