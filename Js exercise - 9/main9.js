9dc099d00dc23fc51b2de63bd1e073a0e9cbb669
MrMacamar <macamartech@gmail.com>
2026-03-19 23:04:04 +0100

    exercise 9

---
+++ Js exercise - 9/main9.js
@@ -0,0 +1,15 @@
+let car = {
+    brand: "Porche",
+    model: "Turbo GR3",
+    year: 2026,
+    
+    start: function(){
+        console.log("The car has started");
+    }
+}
+
+console.log(car.start());
+
+
+
+
