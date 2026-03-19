ab0882cfd5a4d566708114ff03c047feeb87389f
MrMacamar <macamartech@gmail.com>
2026-03-19 23:56:13 +0100

    exercise 10

---
+++ main10.js
@@ -0,0 +1,37 @@
+// Library array object
+
+let library = [
+    // book-1
+    {
+        title: "Clean code",
+        author: "Robert C. Martin",
+        year: 2008
+    },
+    // book-2
+    {
+        title: "The Pragmatic Programmer",
+        author: "Andrew Hunt",
+        year: 2019
+    },
+    // book-3
+    {
+        title: "Design Patterns",
+        author: "Erich Gamma",
+        year: 1994
+    },
+    // book-4
+    {
+        title: "Refactoring: Improving the Design of Existing Code",
+        author: "Martin Fowler",
+        year: 2018
+    },
+    // book-5
+    {
+        title: "Code Complete",
+        author: "Steve McConnell",
+        year: 2004
+    }
+]
+
+console.log(library[0].title);
+console.log(library[1].author);
