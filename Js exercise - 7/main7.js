ab480423a40daf20311bdfb11d7ec97af6cf2ccc
MrMacamar <macamartech@gmail.com>
2026-03-19 21:10:02 +0100

    exercise 7

---
+++ Js exercise - 7/main7.js
@@ -0,0 +1,37 @@
+//  Array declaration
+
+let colors = ["Blue", "Black", "White"];
+
+console.log(colors);
+
+
+// push
+
+console.log("Push section");
+colors.push("Grey");
+console.log(colors);
+
+// Pop
+
+console.log("Push section");
+colors.pop();
+console.log(colors);
+
+// Unshift 
+
+console.log("Unshift section");
+colors.unshift("Purple");
+console.log(colors);
+
+
+// Shift
+
+console.log("Shift section");
+colors.shift()
+console.log(colors);
+
+
+// Length
+
+console.log("Length section");
+console.log(colors.length);
