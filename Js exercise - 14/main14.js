13aaaa86ceb293e3ebc7993961316dce09db6abc
MrMacamar <macamartech@gmail.com>
2026-03-20 23:08:27 +0100

    exercise 14

---
+++ Js exercise - 14/main14.js
@@ -0,0 +1,10 @@
+// Do while loop which forces the user to enter a number greater than 10
+
+
+let number;
+
+do{
+    number = prompt("Xariifow number 10 ka yar ha galin halkaan: ");
+}while(number < 10); // where magic happens
+
+console.log("So dhawoow xariif");
