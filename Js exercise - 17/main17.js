3fe021fbd78b82a552d671f25df36891f942a059
MrMacamar <macamartech@gmail.com>
2026-04-05 15:07:25 +0200

    exercise 17

---
+++ Js exercise - 17/main17.js
@@ -0,0 +1,11 @@
+let weather = 17;
+
+if(weather < 0){
+    console.log("The weather is very Cold 🥶");
+}else if (weather > 0 && weather < 15){
+    console.log("The weather is Cold ❄️");
+}else if (weather > 15 && weather < 20){
+    console.log("The weather is Warm 🌤️");
+}else {
+    console.log("The weather is Hot 🥵");
+}
