e0b27ab33756b037bda2263a8df49e93721dbbc2
MrMacamar <macamartech@gmail.com>
2026-06-14 00:35:16 +0200

    exercise-32

---
+++ Js exercise - 32/main32.js
@@ -0,0 +1,13 @@
+// QuerySelector vs QuerySelectorAll
+// ----------------------------------------
+
+
+const bio = document.querySelector('h1');
+console.log(bio);
+
+const name = document.querySelector('#name');
+console.log(name);
+
+
+const desc = document.querySelectorAll('.info');
+console.log(desc);
