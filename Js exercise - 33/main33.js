94c397a3eff8c7f7a83d54e11a1a44289d7d6421
MrMacamar <macamartech@gmail.com>
2026-06-14 01:58:45 +0200

    exercise 33

---
+++ Js exercise - 33/main33.js
@@ -0,0 +1,18 @@
+// NAME CHANGE
+// --------------------------------------
+
+function changeName(){
+    magac.textContent = "Code PaPa";
+}
+
+// ELEMENT
+// ------------------------------------
+function changeElement(){
+
+    
+    // change Element
+    hoppy.innerHTML = "Code for <strong>Everything</strong>";
+    
+}
+
+
